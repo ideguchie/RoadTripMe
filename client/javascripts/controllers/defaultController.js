0f843c514ad6c74863895c286bc69d70d41bb4b9
Lagisetr <rithvikl@gmail.com>
2016-01-30 13:30:37 -0800

    added google maps stuff

--- client/javascripts/controllers/defaultController.js
+++ client/javascripts/controllers/defaultController.js
@@ -1,10 +1,10 @@
-mainModule.controller('defaultController', function($scope, $routeParams, defaultFactory) {
+mainModule.controller('defaultController', function($scope, $routeParams, defaultFactory, $http) {
 
 	var getAllUsers = function(){
-		console.log("Controller - getAllUsers");
+		// console.log("Controller - getAllUsers");
 		defaultFactory.getAllUsers(function(users){
-			console.log("Back to controller, done with factory by a callback function");
-			console.log(users);
+			// console.log("Back to controller, done with factory by a callback function");
+			// console.log(users);
 			$scope.users = users;
 		});
 	}
@@ -12,4 +12,38 @@ mainModule.controller('defaultController', function($scope, $routeParams, defaul
 	getAllUsers();
 
 	$scope.test = 'abc';
+
+	var getRoute = function(directionsService, directionsDisplay) {
+		console.log("Controller - getRoute");
+
+	  var directionsService = new google.maps.DirectionsService;
+	  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+	  directionsService.route({
+	    origin: "seattle, wa",
+	    destination: "portland, or",
+	    travelMode: google.maps.TravelMode.DRIVING
+	  }, function(response, status) {
+	    if (status === google.maps.DirectionsStatus.OK) {
+	      directionsDisplay.setDirections(response);
+				console.log(response.routes[0].overview_path);
+				getCities(response.routes[0].overview_path);
+	    } else {
+	      window.alert('Directions request failed due to ' + status);
+	    }
+	  });
+	}
+	getRoute();
+
+	var getCities = function(latLongs) {
+		console.log("Controller - getCities");
+		var cities = [];
+		for(var i = 0; i < latLongs.length; i++) {
+			$http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLongs[i].lat() + "," + latLongs[i].lng() + "&key=AIzaSyDdevJaZwheD-E6s1g0r-66f147zHLvYp4").success(function(return_data){
+				cities.push(return_data);
+			});
+		}
+		console.log(cities);
+		// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
+	}
 });

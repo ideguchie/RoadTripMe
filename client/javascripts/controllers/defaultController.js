dfc25ce57dd78852a2d85a4f2ef18f75f72ca7c8
Lagisetr <rithvikl@gmail.com>
2016-01-30 14:53:37 -0800

    stuff

--- client/javascripts/controllers/defaultController.js
+++ client/javascripts/controllers/defaultController.js
@@ -20,8 +20,8 @@ mainModule.controller('defaultController', function($scope, $routeParams, defaul
 	  var directionsDisplay = new google.maps.DirectionsRenderer;
 
 	  directionsService.route({
-	    origin: "seattle, wa",
-	    destination: "portland, or",
+	    origin: "seattle, wa, usa",
+	    destination: "portland, or, usa",
 	    travelMode: google.maps.TravelMode.DRIVING
 	  }, function(response, status) {
 	    if (status === google.maps.DirectionsStatus.OK) {
@@ -38,17 +38,34 @@ mainModule.controller('defaultController', function($scope, $routeParams, defaul
 	var getCities = function(latLongs) {
 		console.log("Controller - getCities");
 		var cities = [];
-		console.log(latLongs[0].lat());
-		console.log(latLongs[0].lng());
-		for(var i = 0; i < latLongs.length; i++) {
-			console.log(latLongs[i].lat());
-			console.log(latLongs[i].lng());
+		var addComps = [];
+
+		for(var i = 0; i < latLongs.length; i += 20) {
 			var pointLat = latLongs[i].lat();
 			var pointLng = latLongs[i].lng();
-			$http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + pointLat + "," + pointLng + "&key=AIzaSyDdevJaZwheD-E6s1g0r-66f147zHLvYp4")
-				.success(function(return_data){
-				cities.push(return_data);
-			});
+			var cityString = "";
+
+			$http({
+			  method: 'GET',
+			  url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + pointLat + "," + pointLng + "&key=AIzaSyDdevJaZwheD-E6s1g0r-66f147zHLvYp4"
+			}).then(function successCallback(response) {
+			    // this callback will be called asynchronously when the response is available
+
+					console.log(response.data);
+
+					// addComps = response.data.results[0].address_components;
+					// for (var j = 0; j < addComps.length; j++) {
+					// 	if(addComps[j].types.indexOf("locality") != -1) {
+					// 		cityString += addComps[j].long_name;
+					// 	}
+					// 	if(addComps[j].types.indexOf("administrative_area_level_1") != -1) {
+					// 		cityString += ", " + addComps[j].short_name;
+					// 	}
+					// }
+
+			  }, function errorCallback(response) {
+					console.log("failed to get cities");
+			  });
 		}
 		console.log(cities);
 	}

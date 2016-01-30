83ba9f3e4032697af4cd4368a90ab2377552903e
Elliot <ideguchie@gmail.com>
2016-01-30 13:21:48 -0800

    increasing data scrub

--- client/javascripts/factories/defaultFactory.js
+++ client/javascripts/factories/defaultFactory.js
@@ -3,12 +3,18 @@ mainModule.factory('defaultFactory', function($http) {
 	var users = [];
 
 	factory.getAllUsers = function(callback){
+		var title = "";
+		var image = "";
+		var price = "";
+		var detail = "";
+		var weather = "";
+		var wDescript = "";
 		var id = "";
 		var lat = "";
 		var lon = "";
 		var active = [];
-		console.log("Factory - getAllUsers");
-		console.log("Executing $http.get getAllUsers");
+		// console.log("Factory - getAllUsers");
+		// console.log("Executing $http.get getAllUsers");
 
 		//get TTDs
 		$http.get('http://terminal2.expedia.com/x/activities/search?location=London&apikey=KvTSobGaExiwiazfRdtoMYpNaRhBk2E9').success(function(returned_data_from_server){
@@ -20,19 +26,27 @@ mainModule.factory('defaultFactory', function($http) {
 				// console.log(id);
 
 				id = active[i].id;
+				title = active[i].title;
+				image = active[i].imageUrl;
+				price = active[i].fromPrice;
 				//get TTD description
-				$http.get("http://terminal2.expedia.com/x/activities/details?activityId="+ id +"&apikey=KvTSobGaExiwiazfRdtoMYpNaRhBk2E9").success(function(returned_data_from_server){
-					console.log("Server responded with: ", returned_data_from_server);
-					lat = returned_data_from_server.latLng.split(",");
+				$http.get("http://terminal2.expedia.com/x/activities/details?activityId="+ id +"&apikey=KvTSobGaExiwiazfRdtoMYpNaRhBk2E9").success(function(returned_data_from_server1){
+					console.log("Server responded with: ", returned_data_from_server1);
+					detail = returned_data_from_server1.metaDescription;
+					lat = returned_data_from_server1.latLng.split(",");
 					lon = lat[1];
 					lat = lat[0];
 
 					//get Location weather
-					$http.get("http://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="+ lon +"&APPID=485450c2da837aceda525ff9a4165fe1").success(function(returned_data_from_server){
-						console.log("Server responded with: ", returned_data_from_server);
+					$http.get("http://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="+ lon +"&APPID=485450c2da837aceda525ff9a4165fe1").success(function(returned_data_from_server2){
+						// console.log("Server responded with: ", returned_data_from_server2);
+						weather = returned_data_from_server2.weather[0].main;
+						wDescript = returned_data_from_server2.weather[0].description;
+
 					});
 
 				});
+				console.log(title + "\n" +image + "\n" +price + "\n" +detail + "\n" +weather + "\n" +wDescript + "\n");
 			}
 		});
 	}
